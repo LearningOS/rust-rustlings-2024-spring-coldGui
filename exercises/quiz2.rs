0b42174f0ba7c3082ebb0b80f8e64bc154e90955
cold_guitar <2641437185@qq.com>
2024-04-14 11:20:23 +0800

    function/if/quiz1-2/variables/intro

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -3,7 +3,7 @@
 // This is a quiz for the following sections:
 // - Strings
 // - Vecs
-// - Move semantics
+// - Move se man tic s
 // - Modules
 // - Enums
 //
@@ -30,13 +30,28 @@ pub enum Command {
 
 mod my_module {
     use super::Command;
-
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec! [];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().into()),
+                Command::Append(iter) => {
+                    let mut op=string.to_string();
+                    let mut it=*iter;
+                    while it>0 {
+                        op=op+"bar";
+                        it=it-1;
+                    }
+                    output.push(op);
+                },
+            };
+            // if command == &Command::Uppercase {
+            //     output.push(string.to_uppercase());
+            // }
         }
         output
     }
@@ -45,7 +60,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
@@ -62,3 +77,5 @@ mod tests {
         assert_eq!(output[3], "barbarbarbarbarbar");
     }
 }
+
+fn main() {}
